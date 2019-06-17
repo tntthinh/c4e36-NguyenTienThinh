ddb4dcb5dd4229e358e3aa79f02bf5a4bdb9ba23
tntthinh <thinh260798@gmail.com>
2019-06-17 22:05:20 +0700

    fisrt ac

---
+++ session1/index.js
@@ -0,0 +1,23 @@
+// let x = Number(prompt("Enter X:"));
+// let y = Number(prompt("Enter Y:"));
+// console.log(x+y);
+
+console.log("Bai 1")
+for(let y=1;y<=100;y++) {
+    console.log(y);
+}
+
+console.log("Bai 2")
+for(let x=1;x<=100;x=x+5){
+    console.log(x);
+}
+
+console.log("Bai 3")
+for (let a =100;a>0;a--) {
+    console.log(a);    
+}
+
+console.log("Bai 4")
+for (let b=100;b>0;b=b-3){
+    console.log(b);
+}
