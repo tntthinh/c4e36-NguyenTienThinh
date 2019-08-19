fdd1babb06ef5b8509d4ce8846e950861dcc6a89
tntthinh <thinh260798@gmail.com>
2019-08-19 13:33:03 +0700

    Emotional Music

--- Emotional Music/main.js
+++ Emotional Music/main.js
@@ -1,17 +1,113 @@
-
+let usSad = [`<iframe width="853" height="480" src="https://www.youtube.com/embed/R9Y3-PxiD7srel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe src="https://player.vimeo.com/video/22823161?autoplay=1&byline=0&portrait=0" width="640" height="344" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>`,
+`<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/131747197&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>`,
+`<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/61657389&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>`,
+`<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/234922478&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>`,
+`<iframe width="640" height="480" src="https://nhac.vn/bai-hat/soledad-westlife-soQAlX4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/428308773&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>`,
+`<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/659841590&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>`,
+`<iframe
+scrolling="no"
+width=640
+height=180
+src=https://zingmp3.vn/embed/song/IW806DBC?start=true frameborder="0" allowfullscreen="true" />`,
+`<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/76517851&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>`
+]
+let usHap = ['<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/652911191&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>',
+`<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/263741978&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>`,`<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/27803131&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/MOWDb2TBYDg?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/vBGiFtb8Rpw?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/ZLrh7hmZtKo?list=PLBE5MMhE6iC1V6QUt0STW1ejA44DELUjs?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/DRt2j4u22jE?list=PLYOH0T-iL1cUgWtQv5K_RQ_Fq_0kIbAw5?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/kivuDS-6HbQ?list=PLYOH0T-iL1cUgWtQv5K_RQ_Fq_0kIbAw5?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/w0EF3AxJwLU?list=PLrHtihXw8ZyrlK0D2B1RPiF-RUqeXo2xe?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/ucs1af3Ntf8?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/BB3OGwplrLo?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/4Uw8EcNmLC4?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+]
+let usIns = [`<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/196259216&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>`,
+`<iframe width="688" height="387" src="https://www.youtube.com/embed/3nQNiWdeH2Q?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="688" height="387" src="https://www.youtube.com/embed/K4DyBUG242c?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/d4KpDhdUcA8?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/Lk-yBArA9fg?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/AQ0resnXCcE?list=PLBE5MMhE6iC1V6QUt0STW1ejA44DELUjs?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/mfJhMfOPWdE?list=PLYOH0T-iL1cUgWtQv5K_RQ_Fq_0kIbAw5?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/BQW7R8d9aww?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/MBu_jP-MlBY?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> `,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/uA51eLELZqE?list=PL-wlqOPEPCpMM9MdO1vVcLpyTOjiBw88V?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+]
+let vSad = [
+`<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/283909932&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/y3o7Dg9Ikpc?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/V6pLnQdGA_c?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/F5tS5m86bOI?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/u0x4bp6tWXs?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/dh2bpIuDz8U?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/MrdQvGx2Dvs?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/aOXvyd9v1cg?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/EYJimpsoE3g?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+]
+let vHap = [`<iframe
+scrolling="no"
+width=640
+height=180
+src=https://zingmp3.vn/embed/song/ZW7W6ZA7?start=true frameborder="0" allowfullscreen="true" />`,
+`<iframe width="847" height="480" src="https://www.youtube.com/embed/3GYjXUBSIss?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="875" height="365" src="https://www.youtube.com/embed/HXkh7EOqcQ4?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="824" height="435" src="https://www.youtube.com/embed/nIl_gX9W5qQ?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/knW7-x7Y7RE?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/JAhdeizXpaQ?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/YlPH5jWbTpA?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="875" height="369" src="https://www.youtube.com/embed/x5CNJUMJcII?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/YUAmi7Q2F0Y?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`]
+let vIns = [`<iframe width="853" height="480" src="https://www.youtube.com/embed/0yE4wEi8CFM?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/5e7e_KZINA4?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/ddaEtFOsFeM?list=PLvHpgQ45HvReBNkpy_R7weN4s3Pzeij4u?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/5Jm9g0YdGDU?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="875" height="354" src="https://www.youtube.com/embed/rtviC6i42bc?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/JqWuzvSMs08?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/ak0IEf3Hpew?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/H6xMQxQIG3Y?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/wDGvlQRoIas?list=PLpAwiDR3-1hjgLQHbBT6DisJY9jjgs9Bv?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+`<iframe width="853" height="480" src="https://www.youtube.com/embed/XIOPBQhOjhk?rel=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`]
+let gen = document.getElementById(`genres`)
+let emo = document.getElementById(`emotion`)
 let playBtn = document.getElementById(`play`)
 playBtn.addEventListener('click',()=>{
-    let a = document.getElementById(`vi`)
-    let vid = document.getElementById(`container`)
-    let add = 
-    `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/515807238&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`
-    vid.innerHTML+=add
-    a.innerHTML+=add;
+    let vid = document.getElementById("show")
+    console.log("ok")
+    let x = Math.floor(Math.random() * 10);
+    if(gen.value === "US/UK" && emo.value==="sad"){
+        document.body.style.background = "url('https://i.imgur.com/9WgJg0G.gif')"
+        document.body.style.backgroundSize = "cover"
+        document.body.style.transition = "6s"
+        vid.innerHTML+=usSad[x]
+    console.log("usad")}
+    else if(gen.value === "US/UK" && emo.value==="happy"){
+        document.body.style.background = "url('https://wallpapercave.com/wp/wp2757890.gif')";
+        document.body.style.backgroundSize = "cover"
+        document.body.style.transition = "6s"
+        vid.innerHTML+=usHap[x]
+    console.log("uhap")}
+    else if(gen.value === "US/UK" && emo.value==="ins"){
+        document.body.style.background = "url('https://wallpaperaccess.com/full/723262.gif')"
+        document.body.style.backgroundSize = "cover"
+        document.body.style.transition = "6s"
+        vid.innerHTML+=usIns[x]}
+    else if(gen.value === "Vpop" && emo.value==="sad"){
+        document.body.style.background = "url('https://i.imgur.com/9WgJg0G.gif')"
+        document.body.style.backgroundSize = "cover"
+        document.body.style.transition = "6s"
+        vid.innerHTML+=vSad[x]}
+    else if(gen.value === "Vpop" && emo.value==="happy"){
+        document.body.style.background = "url('https://wallpapercave.com/wp/wp2757890.gif')";
+        document.body.style.backgroundSize = "cover"
+        document.body.style.transition = "6s"
+        vid.innerHTML+=vHap[x]}
+    else if(gen.value === "Vpop" && emo.value==="ins"){
+        document.body.style.background = "url('https://wallpaperaccess.com/full/723262.gif')"
+        document.body.style.backgroundSize = "cover"
+        document.body.style.transition = "6s"
+        vid.innerHTML+=vIns[x]}
 })
 
-let a = ()=> {
-    var cmb = document.getElementById("genres");
-    document.getElementById("US/UK").value = cmb.value;
-    document.getElementById("Vpop").value = cmb.options[cmb.selectedIndex].text;
-    
-}
